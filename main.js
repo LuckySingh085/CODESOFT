16de1220385b72aaed6332508f9e44a7fb940567
LUCKY SINGH <085luckysingh@gmail.com>
2024-11-28 20:01:15 +0530

    LANDING PAGE
    LEVEL 1   TASK 2 
     WEB DEVELOPMENT INTERNSHIP by  CODESOFT

---
+++ main.js
@@ -0,0 +1,25 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: bottom,
+    duration: 1000,
+};
+scrollReveal().reveal(".header_image img," {
+    ...scrollRevealOption,
+    origin: "right",
+
+});
+scrollReveal().reveal(".header_contant h1," {
+    ...scrollRevealOption,
+    dealy: 500,
+
+});
+scrollReveal().reveal(".header_contant p," {
+    ...scrollRevealOption,
+    dealy: 1000,
+
+});
+scrollReveal().reveal(".header_contant form" {
+    ...scrollRevealOption,
+    dealy: 1500,
+
+});
